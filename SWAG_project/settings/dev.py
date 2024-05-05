3152d3f13a602ddbf392361b2d26d818c42b201a
Richard Emmanuel <richardemmanuel45@gmail.com>
2024-05-05 18:02:00 +0100

    :zap: keys deactivation

--- SWAG_project/settings/dev.py
+++ SWAG_project/settings/dev.py
@@ -22,7 +22,7 @@ FLUTTERWAVE_SECRET_KEY = config("FLUTTERWAVE_SECRET_KEY")
 # Email
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL=config('EMAIL_USE_SSL')
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'swag4africa@gmail.com'
-EMAIL_HOST_PASSWORD = 'cdzcqqnaypltrebb'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
